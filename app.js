5ffecece0e13660ed51568d1191bbc1dce023184
Edson Câmara Filho <edsoncamarafilho07@gmail.com>
2023-11-05 23:54:24 -0300

    [Fix] Arrumando identação entre questões

--- app.js
+++ app.js
@@ -10,9 +10,9 @@
     const driverAge1 = 18
 
     if (driverAge1 >= 18) {
-        document.write(`Pode dirigir`)
+        document.write(`Pode dirigir <br><br>`)
     } else {
-        document.write(`Não pode dirigir`)
+        document.write(`Não pode dirigir<br><br>`)
     }
 
 
@@ -29,9 +29,9 @@
     let license = confirm ('Tem CNH ou não?')
 
     if (driverAge >= 18 && license === true) {
-        document.write(`Pode dirigir`)
+        document.write(`Pode dirigir<br><br>`)
     } else {
-        document.write(`Não pode dirigir`)
+        document.write(`Não pode dirigir<br><br>`)
     }
 
 
@@ -46,34 +46,34 @@
     const days1 = Number(prompt(`Digite um número de 1 a 7`))
 
     if (days1 === 1) {
-        document.write(`O dia correspondente é domingo`)
+        document.write(`O dia correspondente é domingo<br><br>`)
 
     } else if (days1 === 2) {
 
-        document.write(`O dia correspondente é segunda`)
+        document.write(`O dia correspondente é segunda<br><br>`)
 
     } else if (days1 === 3) {
 
-        document.write(`O dia correspondente é terça`)
+        document.write(`O dia correspondente é terça<br><br>`)
 
     } else if (days1 === 4) {
 
-        document.write(`O dia correspondente é quarta`)
+        document.write(`O dia correspondente é quarta<br><br>`)
 
     } else if (days1 === 5) {
 
-        document.write(`O dia correspondente é quinta`)
+        document.write(`O dia correspondente é quinta<br><br>`)
 
     } else if (days1 === 6) {
 
-        document.write(`O dia correspondente é sexta`)
+        document.write(`O dia correspondente é sexta<br><br>`)
 
     } else if (days1 === 7) {
 
-        document.write(`O dia correspondente é sábado`)
+        document.write(`O dia correspondente é sábado<br><br>`)
 
     } else  {
-        document.write(`Dia não reconhecido`)
+        document.write(`Dia não reconhecido<br><br>`)
     }
 
 
@@ -87,25 +87,25 @@
 
     switch (days) {
         case 1:
-            document.write(`O dia correspondente é domingo`)
+            document.write(`O dia correspondente é domingo<br><br>`)
         break;
         case 2:
-            document.write(`O dia correspondente é segunda`)
+            document.write(`O dia correspondente é segunda<br><br>`)
         break;
         case 3:
-            document.write(`O dia correspondente é terça`)
+            document.write(`O dia correspondente é terça<br><br>`)
         break;
         case 4:
-            document.write(`O dia correspondente é quarta`)
+            document.write(`O dia correspondente é quarta<br><br>`)
         break;
         case 5:
-            document.write(`O dia correspondente é quinta`)
+            document.write(`O dia correspondente é quinta<br><br>`)
         break;
         case 6:
-            document.write(`O dia correspondente é sexta`)
+            document.write(`O dia correspondente é sexta<br><br>`)
         break;
         case 7:
-            document.write(`O dia correspondente é sábado`)
+            document.write(`O dia correspondente é sábado<br><br>`)
         break;
         default:
             document.write(`Dia não reconhecido`)
@@ -121,7 +121,7 @@
 
     for (let i = 1; i <= 10 ; i++) {
         if (i %2 == 0) {
-            document.write(` ${i}`) 
+            document.write(` ${i}<br><br>`) 
         }
     }
 
@@ -138,7 +138,7 @@ let contador = 0
 
 for (let i = 1; i <= 60 ; i++) {
     if (i %2 != 0) {
-        document.write(` ${i}`)
+        document.write(` ${i}<br><br>`)
         contador++ 
     }
 } document.write(`<br> Foram impressos ${contador} números impares.`)
@@ -160,7 +160,7 @@ do {
     counter1++
 } while (counter1 >= 10 && counter1 <= 100)
 
-document.write(`${sum1}`)
+document.write(`${sum1}<br><br>`)
 
 
 
@@ -177,9 +177,9 @@ document.write(`${sum1}`)
     let salarioUsuario = Number(prompt('Digite o seu salário'))
 
     if(salarioUsuario < 1903.98) {
-        document.write(`Inseto de Imposto de Renda`)
+        document.write(`Inseto de Imposto de Renda<br><br>`)
     } else {
-        document.write(`Tributado no Imposto de Renda`)
+        document.write(`Tributado no Imposto de Renda<br><br>`)
     }
 
 
@@ -197,10 +197,10 @@ document.write(`${sum1}`)
     let numero = 257
 
     if(numero <= 1 ){
-        console.log('Não é primo')
+        document.write('Não é primo<br><br>')
 
     } else if(numero > 2 && numero %2 === 0) {
-        console.log('Não é primo')
+        document.write('Não é primo<br><br>')
     } else {
         let vezesZero = 0
 
@@ -208,14 +208,14 @@ document.write(`${sum1}`)
             if(numero %i === 0) {
                 vezesZero++
                 if (vezesZero === 3) {
-                    console.log('Não é primo')
+                    document.write('Não é primo<br><br>')
                     break
                 }
             } 
         }
 
         if (vezesZero === 2 ) {
-            console.log('O numero é primo')
+            document.write('O numero é primo<br><br>')
         }
     }
 
@@ -234,15 +234,15 @@ document.write(`${sum1}`)
 
     if(genero == 1) {
         pesoIdeal = ((altura * 62.1) - 44.7).toFixed(2)
-        document.write(`Seu peso ideal é ${pesoIdeal}`)
+        document.write(`Seu peso ideal é ${pesoIdeal}<br><br>`)
     }
 
     else if (genero == 2) {
         pesoIdeal = ((altura * 72.7) - 58).toFixed(2)
-        document.write(`Seu peso ideal é ${pesoIdeal}`)
+        document.write(`Seu peso ideal é ${pesoIdeal}<br><br>`)
         
     } else {
-        document.write(`Digite um número válido`)
+        document.write(`Digite um número válido<br><br>`)
     }
 
 
@@ -287,9 +287,9 @@ let horarioEstudo = prompt(`Digite o horário que você estuda, M para matutino,
     let doarSangue = Number(prompt('Digite a sua idade'))
 
     if(doarSangue >=67 && doarSangue <= 18 ) {
-        document.write('Você não pode doar sangue')
+        document.write('Você não pode doar sangue <br><br>')
     } else {
-        document.write('Você pode doar sangue')
+        document.write('Você pode doar sangue <br><br>')
     }
 
 
@@ -333,7 +333,7 @@ while (soma < 500) {
   soma += ultimoValor;
 }
 
-document.write(`O último valor multiplicado por 3 é: ${ultimoValor}`);
+document.write(`O último valor multiplicado por 3 é: ${ultimoValor}<br><br>`);
 
 
 
@@ -367,16 +367,16 @@ document.write(`O último valor multiplicado por 3 é: ${ultimoValor}`);
 
     if (a <(b + c) && b < (a + c) && c < (a + b)){25
         if(a == b && a == c && b == c) {
-            document.write('É um triângulo equilátero.')
+            document.write('É um triângulo equilátero.<br><br>')
         }
         else if( a == b || a == c || b == c) {
-            document.write('É um triângulo isóceles.')
+            document.write('É um triângulo isóceles.<br><br>')
         
         } else if (a != b && a != c && b != c) {
-            document.write('É um triângulo escaleno.')
+            document.write('É um triângulo escaleno.<br><br>')
         }
     }else{
-        document.write('Não é um triângulo')
+        document.write('Não é um triângulo<br><br>')
     }
 
 
@@ -395,9 +395,9 @@ document.write(`O último valor multiplicado por 3 é: ${ultimoValor}`);
     const calculoVotar = (anoAtual - anoDeNascimento)
 
     if (calculoVotar >= 16) {
-        document.write ('Pode votar!')
+        document.write ('Pode votar!<br><br>')
     } else {
-        document.write ('Você não pode votar!')
+        document.write ('Você não pode votar!<br><br>')
     }
 
 
@@ -419,7 +419,7 @@ document.write(`O último valor multiplicado por 3 é: ${ultimoValor}`);
     let votosNulos = Number(prompt(`Digite o total de VOTOS NULOS:`))
 
     if (votosBrancos >= votosTotais || votosNulos >= votosTotais || (votosNulos + votosBrancos) >= votosTotais) {
-        document.write("Quantidade de votos INVÁLIDAS")
+        document.write("Quantidade de votos INVÁLIDAS<br><br>")
     } else {
         const percenValidatedVotes = ((votosTotais - votosBrancos - votosNulos) / votosTotais) * 100
         const percenvotosBrancos = (votosBrancos / votosTotais) * 100
@@ -465,7 +465,7 @@ document.write(`O último valor multiplicado por 3 é: ${ultimoValor}`);
     } else {
         document.write(`Pagamento INVÁLIDO <br />`)
     }
-    document.write(`O preço final de sua compra ficou: R$${precoFinal}`)
+    document.write(`O preço final de sua compra ficou: R$${precoFinal}<br><br>`)
 
     
 
